b6bc750af4bada32cce747cabf72b3802902c90a
gl <gaoguwen2001@hotmail.com>
2019-05-07 09:04:27 +0800

    修改Test2.java类

--- src/main/java/com/qst/bx_web/controller/IndexController.java
+++ src/main/java/com/qst/bx_web/controller/IndexController.java
@@ -1,15 +1,24 @@
 package com.qst.bx_web.controller;
 
+import com.qst.bx_web.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("index")
-    public String index(Model model){
-        model.addAttribute("welcome","欢迎访问我的网站！");
+    public String index(Model model, HttpSession session) {
+        String username = session.getAttribute("user") != null ? ((User) session.getAttribute("user")).getUsername() : "";
+        model.addAttribute("welcome", "欢迎" + username + "访问我的网站！");
         return "/index";
+    }
+
+    @RequestMapping("login")
+    public String login(Model model) {
+        return "/login";
     }
 }
