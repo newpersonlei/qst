e26da8656c337dbd97391935b4f703bf67c62626
gl <gaoguwen2001@hotmail.com>
2019-05-09 16:31:47 +0800

    添加Layout模板页功能

--- src/main/java/com/qst/bx_web/controller/IndexController.java
+++ src/main/java/com/qst/bx_web/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.qst.bx_web.controller;
 
+import com.qst.bx_web.pojo.Category;
 import com.qst.bx_web.pojo.User;
+import com.qst.bx_web.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,17 @@ import java.util.List;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private CategoryService categoryService;
 
     @RequestMapping("index")
     public String index(Model model, HttpSession session) {
         String username = session.getAttribute("user") != null ? ((User) session.getAttribute("user")).getUsername() : "";
         model.addAttribute("welcome", "欢迎" + username + "访问我的网站！");
+        if (session.getAttribute("categorylist") == null) {
+            List<Category> categoryList = categoryService.getAllCategorys();
+            session.setAttribute("categorylist", categoryList);
+        }
         return "/index";
     }
 
@@ -37,4 +46,10 @@ public class IndexController {
     public String bookdetails(Model model) {
         return "/bookdetails";
     }
+
+    @RequestMapping("basket")
+    public String basket(Model model) {
+        return "/basket";
+    }
+
 }
